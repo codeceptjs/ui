4e8ceb79b8c5bef1141fa0406db0fde9fc0b9020
Stefan Huber <hubidu@users.noreply.github.com>
2019-07-31 18:18:32 +0200

    Customization

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,43 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  color: #2c3e50;
   box-sizing: border-box;
   -moz-osx-font-smoothing: grayscale;
   font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
   -webkit-font-smoothing: antialiased;
 }
+
+@import "~bulma/sass/utilities/_all";
+
+// Set your colors
+$primary: green;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099FF;
+$twitter-invert: findColorInvert($twitter);
+
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+    "white": ($white, $black),
+    "black": ($black, $white),
+    "light": ($light, $light-invert),
+    "dark": ($dark, $dark-invert),
+    "primary": ($primary, $primary-invert),
+    "info": ($info, $info-invert),
+    "success": ($success, $success-invert),
+    "warning": ($warning, $warning-invert),
+    "danger": ($danger, $danger-invert),
+    "twitter": ($twitter, $twitter-invert)
+);
+
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+
 </style>

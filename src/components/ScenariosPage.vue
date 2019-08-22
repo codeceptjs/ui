90ea93a33f6ffb6b5771d47d95976f32db5a68a1
Stefan Huber <hubidu@users.noreply.github.com>
2019-08-22 17:45:29 +0200

    Show error toast when tests can not be loaded due to syntax error

--- src/components/ScenariosPage.vue
+++ src/components/ScenariosPage.vue
@@ -113,7 +113,22 @@ export default {
   sockets: {
     'codeceptjs:scenarios.updated': function () {
       this.loadProject();
-    }
+
+      this.$buefy.toast.open({
+        message: 'Scenarios have been reloaded',
+        type: 'is-success'
+      })
+    },
+    'codeceptjs:scenarios.parseerror': function (err) {
+      const stackFrames = err.stack.split('\n');
+
+      this.$buefy.toast.open({
+        duration: 10000,
+        message: `"${err.message}"\n${stackFrames[0]} in ${stackFrames[1]}`,
+        position: 'is-top',
+        type: 'is-danger'
+      });
+    },
   },
   methods: {
       selectMatchType(t) {
--- src/store/modules/scenarios.js
+++ src/store/modules/scenarios.js
@@ -1,6 +1,7 @@
 const scenarios = {
   namespaced: true,
   state: {
+    parseError: undefined,
     search: '',
     selectedScenario: undefined,
   },
@@ -8,12 +9,21 @@ const scenarios = {
     search: (state) => state.search
   },
   mutations: {
+    setParseError: (state, err) => {
+      state.parseError = err;
+    },
     setSearch: (state, search) => {
       state.search = search;
     },
     selectScenario: (state, scenario) => {
       state.selectedScenario = scenario;
     }
+  },
+  actions: {
+    'SOCKET_codeceptjs:scenarios.parseerror': function ({ commit }, err) {
+      commit('setParseError', err);
+    },
+
   }
 }
 

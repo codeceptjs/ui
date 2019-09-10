9ea5d186f5f99e39f43840fd7631d1741c49334f
avinash360 <avithakur86@gmail.com>
2019-09-10 23:00:36 +0530

    add alt to image

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
             <a class="navbar-item" href="/">
-                <img src="../assets/logo.png">
+                <img src="../assets/logo.png" alt="codepress logo">
                 &nbsp;
                 codepress
             </a>

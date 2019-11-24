63e646c0e1526b9583d27f89152e285efe7f0543
AndreyKatrusha <df117879@gmail.com>
2019-11-25 01:25:32 +0200

    fix

--- src/components/SettingsPage.vue
+++ src/components/SettingsPage.vue
@@ -4,12 +4,20 @@
       <h1 class="title">Settings</h1>
 
       To be implemented...
+      <button class="button is-primary m-1" @click="goToBack()">
+       Back
+      </button>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'SettingsPage'
+  name: 'SettingsPage',
+  methods: {
+    goToBack() {
+      this.$router.go(-1);
+    },
+  },
 }
 </script>

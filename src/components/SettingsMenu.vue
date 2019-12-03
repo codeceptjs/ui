11db2e5e53492b2eda39b76803426fbac194fc4b
PeterNgTr <peter.nguyentr@gmail.com>
2019-12-03 17:30:42 +0100

    fix error when navigating to config file while sitting on test page

--- src/components/SettingsMenu.vue
+++ src/components/SettingsMenu.vue
@@ -248,7 +248,7 @@ export default {
       this.$store.dispatch('settings/setHeadless', this.headlessMode);
     },
     gotoSettingsPage() {
-      this.$router.push('settings');
+      this.$router.push({path: '/settings'});
     }
   }
 };

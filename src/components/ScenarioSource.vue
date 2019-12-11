b87dfa0b368e7d4be94075e9da5053f55f01f17e
Michael Bodnarchuk <DavertMik@users.noreply.github.com>
2019-12-11 15:03:48 +0200

    Added better description for error on editor (#42)
    * added better description for error on editor
    
    * updated lanuching bootstrap

--- src/components/ScenarioSource.vue
+++ src/components/ScenarioSource.vue
@@ -9,87 +9,19 @@
     >
       <i class="fas fa-edit" /> Edit Test
     </b-button>
-    <b-modal
-      :active.sync="isOpened"
-      :width="640"
-      scroll="keep"
-    >
-      <div class="card">
-        <div class="card-content">
-          <div class="content">
-            <div class="error">
-              <h1>Error</h1>
-              <h2><span class="error-message">{{ errorMessage }}</span></h2>
-            </div>
-            <div class="error">
-              <h2><span class="error-message">{{ errorMessageDescription }}</span> </h2>
-            </div>
-            <div>
-              <h3>How to solve this, install simple editors:</h3>
-            </div>
-            <ul>
-              <li>
-                <div>
-                  <a
-                    href="https://code.visualstudio.com/Download"
-                    target="_blank"
-                  >Download Visual studio code</a>
-                </div>
-                <div>
-                  <a
-                    href="https://code.visualstudio.com/docs/setup/setup-overview"
-                    target="_blank"
-                  >
-                    Setup overview visual studio code
-                  </a>
-                </div>
-                <span>Command  <span style="color: white; background-color: black">code</span></span>
-              </li>
-              <li>
-                <div>
-                  <a href="https://www.jetbrains.com/webstorm/download/">Download Webstorm</a>
-                </div>
-                <div>
-                  <ol>
-                    <li>Try in terminal 'wstorm' and 'webstorm'</li>
-                    <li>If the commands don't work you can run in WebStorm: "Tools" -> "Create Command Line Launcher..."</li>
-                  </ol>
-                  <span>Note: The solution works only for Linux / MacOS</span>
-                  <div>
-                    <span>Command  <span style="color: white; background-color: black">wstorm</span></span>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div>
-                  <a href="https://www.sublimetext.com/"> Sublime text</a>
-                </div>
-              </li>
-            </ul>
-            <div>
-              <p>
-                If you do not have an editor that is called through the console. Or you have a problem with calling him. Then we can offer you a live editor(next update)
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </b-modal>
+    <EditorNotFound
+      :error="error"
+      :is-opened="!!error"
+    />
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import EditorNotFound from './EditorNotFound';
 
 export default {
   name: 'ScenarioSource',
-  data() {
-    return {
-      isOpened: false,
-      errorMessage: '',
-      errorMessageDescription: ''
-    };
-  },
   props: {
     source : {
       type: String,
@@ -100,30 +32,25 @@ export default {
       required: true,
     }
   },
-  components: {},
+  components: {
+    EditorNotFound,
+  },
+  data() {
+    return {
+      error: null,
+    };
+  },
   methods: {
     editFile() {
       axios
         .get(`/api/tests/${encodeURIComponent(this.file)}/open`)
         .then(() => {
-          this.state.errorMessage = '';
-          this.errorMessageDescription = '';
+          this.error = null;
         })
         .catch((error) => {
-          const { response: {
-            data: {
-              message,
-              description
-            }
-          } } = error;
-          this.errorMessage = message;
-          this.errorMessageDescription = description;
-          this.openModal();
+          this.error = error.response.data;
         });
     },
-    openModal() {
-      this.isOpened = !this.isOpened;
-    }
   }
 };
 </script>

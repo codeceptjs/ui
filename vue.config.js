b31ce85fd87e44447f4e8b6f6975322b4dffe8e4
mirhaCode <erwan.guillet44@hotmail.fr>
2019-12-19 20:11:50 +0100

    :bug: Fix command backend and example on windows

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
+const { getPort } = require('./lib/config/env');
+const PORT = getPort() + 1;
+
 module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:3001/',
+        target: `http://localhost:${PORT}`,
       }
     }
   }

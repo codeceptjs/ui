3b182a13abacb0b5f4811f743c5b423202c11cbc
kobenguyent <kobenguyent@gmail.com>
2024-02-26 11:53:42 +0100

    fix: e2e command

--- test/e2e/homepage_test.ts
+++ test/e2e/homepage_test.ts
@@ -2,15 +2,10 @@ const { I } = inject();
 
 Feature('homepage');
 
-BeforeSuite(() => {
+
+Scenario('Home page is loaded',  () => {
   I.amOnPage('');
-})
-
-Scenario('See write a test button',  () => {
   I.waitForText('Write a Test');
-});
-
-Scenario('See scenario detail link',  () => {
   I.waitForElement('.Scenario-detailLink');
 });
 
